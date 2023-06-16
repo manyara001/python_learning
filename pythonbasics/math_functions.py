3aded8ede70251b6e1edc8a9e7c0a801c653feb1
manyara001 <manyisabernard@gmail.com>
2023-06-16 19:05:24 +0300

    number.py

---
+++ pythonbasics/math_functions.py
@@ -0,0 +1,9 @@
+#fundamental data types
+# int and float
+print(type(2 + 2))
+print(type(33 / 34))
+print(55 * 66)
+print(22 - 55)
+print(22 ** 8)
+print(8 // 4)
+print( 6 % 4)
---
+++ pythonbasics/number.py
@@ -0,0 +1,24 @@
+#fundamental data types
+# int and float
+print(type(2 + 2))
+print(type(33 / 34))
+print(55 * 66)
+print(22 - 55)
+print(22 ** 8)
+print(8 // 4)
+print( 6 % 4)
+
+#maths functions
+print(round(3.3))
+print(round(0.1))
+print(abs(-20))
+
+#min and max
+X = min(1, 5, 7, 90, 100 )
+y = max(100, 5, 8 , 89)
+print(X)
+print(y)
+print(min(100, 300, 400, 670))
+
+ #order precidence
+print(5 * 7 / 8 + 9 - 8)
