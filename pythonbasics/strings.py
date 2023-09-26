6eca7bf73fe3c2c429b40344ed931dcd68cc8e62
manyara001 <manyisabernard@gmail.com>
2023-09-26 14:11:24 +0300

    pythonbasics/strings.py

--- pythonbasics/strings.py
+++ pythonbasics/strings.py
@@ -3,3 +3,10 @@ message = "Hello, world!"
 
 # Print the original string
 print("Original message:", message)
+
+#basic calculator
+
+first_number = float(input("what is the firt number?" ))
+second_number = float(input("what is the secodn number? "))
+sum = first_number + second_number
+print(sum)
